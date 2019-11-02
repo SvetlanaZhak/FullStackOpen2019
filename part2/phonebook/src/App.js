2b363e4f48537e1423c8ba3b7bcec2d133a56ba8
SvetlanaZhak <lana.zhak@gmail.com>
2019-11-02 19:46:46 +0200

    step 1- 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,38 +1,75 @@
 import React, { useState } from "react";
 import Person from "./components/Person";
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilter] = useState("");
+
+  const onFilterChange = event => setFilter(event.target.value);
+  const filteredPersons = persons.filter(
+    person => person.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1
+  );
 
   const rows = () =>
-    persons.map((person, index) => <Person key={index} person={person} />);
+    filteredPersons.map((person, index) => (
+      <Person key={index} person={person} />
+    ));
 
   const onNameChange = event => {
     setNewName(event.target.value);
   };
 
+  const onNumberChange = event => {
+    setNewNumber(event.target.value);
+  };
+
+  const checkExistingPer = newName => {
+    let ExistingPer = false;
+    persons.forEach(person =>
+      person.name === newName ? (ExistingPer = true) : (ExistingPer = false)
+    );
+    return ExistingPer;
+  };
+
   const addPerson = event => {
     event.preventDefault();
-    const personObject = {
-      name: newName
-    };
-    setPersons(persons.concat(personObject));
-    setNewName("");
+    if (checkExistingPer(newName) === false) {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      };
+      setPersons(persons.concat(personObject));
+      setNewName("");
+      setNewNumber("");
+    } else {
+      alert(`${newName} is already added to phonebook`);
+    }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-
+      filter shown with
+      <input value={filterName} onChange={onFilterChange} />
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:
           <input value={newName} onChange={onNameChange} />
         </div>
+        <div>
+          number:
+          <input value={newNumber} onChange={onNumberChange} />
+        </div>
         <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-
       {rows()}
     </div>
   );
--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+// const [persons, setPersons] = useState([
+//   { name: "Arto Hellas", number: "040-123456" },
+//   { name: "Ada Lovelace", number: "39-44-5323523" },
+//   { name: "Dan Abramov", number: "12-43-234345" },
+//   { name: "Mary Poppendieck", number: "39-23-6423122" }
+// ]);
 const Person = ({ person }) => {
-  return <p>{person.name}</p>;
+  return (
+    <p>
+      {person.name} {person.number}
+    </p>
+  );
 };
 
 export default Person;
