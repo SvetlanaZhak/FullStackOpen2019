d11cda9cf69d784f22348bba0f45a37f40c366e9
SvetlanaZhak <lana.zhak@gmail.com>
2019-11-16 01:58:42 +0200

    part3 added

--- part3/3b/phonebook/src/components/Persons.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Person from "./Person";
-
-const Persons = ({ persons, setPersons }) => {
-  return persons.map((person, index) => (
-    <Person key={index} person={person} setPersons={setPersons} />
-  ));
-};
-export default Persons;
