ed0f3cb4667bc1437536b7bf7da9057a7986a8fc
SvetlanaZhak <lana.zhak@gmail.com>
2019-12-15 01:50:49 +0200

    Add part a

---
+++ part4/models/blogs.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+
+const blogsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        minlength: 3,
+        required: true,
+
+    },
+    author: {
+        type: String,
+        minlength: 3,
+        required: true,
+
+    },
+    url: String,
+    likes: Number
+
+})
+
+blogsSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogsSchema)
