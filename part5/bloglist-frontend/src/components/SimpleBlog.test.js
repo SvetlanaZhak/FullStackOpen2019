f65cbe98ce41cdd4854cc0c756bbffea9f22834a
SvetlanaZhak <lana.zhak@gmail.com>
2019-12-22 22:20:03 +0200

    5.14

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -19,3 +19,16 @@ test("renders content", () => {
     expect(component.container).toHaveTextContent("Lana")
     expect(component.container).toHaveTextContent('blog has 12 likes')
 })
+
+
+test("clicking likes button twice calls event handler twice", () => {
+    const mockHandler = jest.fn()
+    const { getByText } = render(
+        <SimpleBlog blog={blog} onClick={mockHandler} />
+    )
+    const button = getByText("like")
+    fireEvent.click(button)
+    fireEvent.click(button)
+    expect(mockHandler.mock.calls.length).toBe(2)
+})
+
